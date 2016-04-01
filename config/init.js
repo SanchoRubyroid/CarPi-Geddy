50fa11a7f638c53cc586a4fa18f2fa2f5df2faca
Alexander Areshnikov <alex.areshnikov@plansource.com>
2016-04-01 14:45:22 -0600

    Add initialization logic

--- config/init.js
+++ config/init.js
@@ -1,3 +1,5 @@
+var redis = require("redis")
+
 var init = function(cb) {
 
   // Add uncaught-exception handler in prod-like environments
@@ -15,12 +17,28 @@ var init = function(cb) {
   }
   cb();
 
+  geddy.on('initialized', function() {
+    // redis client setup
+    geddy.redis_cli = redis.createClient({
+      password: geddy.config.redis_pwd,
+      timeout: 0});
+
+    geddy.redis_cli.on("error", function (err) {
+      geddy.log.error("Error " + err);
+    });
+  })
+
   geddy.on('started', function() {
-    geddy.io.sockets.on('connection', function(socket) {
-      socket.emit('hello', {message: "world"});
-      socket.on('message', function(message) {
-        geddy.log.notice(message);
-      });
+    // sockets client setup
+    // geddy.io.sockets.on('connection', function() {
+    //   socket.emit('hello', {message: "world"});
+    //   socket.on('message', function(message) {
+    //     geddy.log.notice(message);
+    //   });
+    // });
+
+    geddy.io.sockets.on('online', function(car_name) {
+      geddy.redis_cli.setex('car'+car_name, 2, '')
     });
   })
 };
