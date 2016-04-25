ad88b0aeacf4ca72fcc49fc0287318b26433773e
Alexander Areshnikov <alex.areshnikov@plansource.com>
2016-04-25 16:29:58 -0600

    Implement 3 byte vehicle control

--- app/controllers/carpi_control.js
+++ app/controllers/carpi_control.js
@@ -2,8 +2,8 @@ var CarpiControl = function () {
   this.index = function (req, resp, params) {
     geddy.io.sockets.on('connection', function(socket) {
       socket.on('car-control', function(data) {
-        var vehicle_socket = geddy.vehicleConnectionUtils.getSocketByName(params['id'])        
-        vehicle_socket.write(JSON.stringify(data))
+        var vehicle_socket = geddy.vehicleConnectionUtils.getSocketByName(params['id'])
+        vehicle_socket.write(new Buffer(data))
       });
     });
 
